ba21da9e0fff0c046ab1b8256f4fbb2c54e49c07
Andreas Prinz <andreas.prinz@outlook.at>
2024-06-03 23:20:56 +0200

    implement calculateWDayData

--- src/pages/api/graphs.ts
+++ src/pages/api/graphs.ts
@@ -16,6 +16,12 @@ interface SingleSensorRow {
   rain: number[];
 }
 
+interface DayCommonSensorRow {
+  average: number[];
+  min: number[];
+  max: number[];
+}
+
 export const GET: APIRoute = async ({ params, request }) => {
   const searchParams = new URLSearchParams(new URL(request.url).search);
   const from = Number(searchParams.get("from"));
@@ -37,9 +43,11 @@ async function getData(from: number, to: number): Promise<GraphData> {
   const stepSize = calculateStepSize((to - from) / intervalSingle, minValues);
   const dataSpacing = stepSize * intervalSingle;
 
-  // if (dataSpacing < threshold) {
-  return await calculateWSingleData(db, from, to, intervalSingle, minValues);
-  // }
+  if (dataSpacing < threshold) {
+    console.log("Single data");
+    return await calculateWSingleData(db, from, to, intervalSingle, minValues);
+  }
+  console.log("Day data");
   return await calculateWDayData(db, from, to, minValues);
 }
 
@@ -55,6 +63,9 @@ async function calculateWSingleData(db: Database, from: number, to: number, inte
   const data = getDummyGraphData();
   const sensors = ["outTemp", "appTemp", "dewpoint", "outHumidity", "barometer", "windspeed", "windDir", "rain"];
   const singleData = await getDataSensors(db, sensors, from, to);
+  if (singleData.dateTime.length === 0) {
+    return data;
+  }
 
   data.outTempMin = (await getDayMin(db, "outTemp", from, to)).map((value) => unitConverter.fahrenheitToCelsius(value, 1));
   data.outTempMax = (await getDayMax(db, "outTemp", from, to)).map((value) => unitConverter.fahrenheitToCelsius(value, 1));
@@ -95,6 +106,56 @@ async function calculateWSingleData(db: Database, from: number, to: number, inte
 
 async function calculateWDayData(db: Database, from: number, to: number, minValues: number): Promise<GraphData> {
   const data = getDummyGraphData();
+  const dateTimes = await getDayDatetimes(db, from, to);
+  if (dateTimes.length === 0) {
+    return data;
+  }
+
+  const shortenedDatetimes = shorten(dateTimes, minValues);
+  if (new Date(shortenedDatetimes[0] * 1000).getFullYear() === new Date(shortenedDatetimes[shortenedDatetimes.length - 1] * 1000).getFullYear()) {
+    data.dateTime = shortenedDatetimes.map((value) => utils.formatDateDDMMHHMM(new Date(value * 1000)));
+  } else {
+    data.dateTime = shortenedDatetimes.map((value) => utils.formatDateDDMMYYHHMM(new Date(value * 1000)));
+  }
+
+  let commonSensorResult;
+  commonSensorResult = await commonDaySensorQuery(db, "outTemp", from, to, minValues, unitConverter.fahrenheitToCelsius, 1);
+  data.outTemp = commonSensorResult.average;
+  data.outTempMin = commonSensorResult.min;
+  data.outTempMax = commonSensorResult.max;
+
+  commonSensorResult = await commonDaySensorQuery(db, "appTemp", from, to, minValues, unitConverter.fahrenheitToCelsius, 1);
+  data.appTemp = commonSensorResult.average;
+  data.appTempMin = commonSensorResult.min;
+  data.appTempMax = commonSensorResult.max;
+
+  commonSensorResult = await commonDaySensorQuery(db, "dewpoint", from, to, minValues, unitConverter.fahrenheitToCelsius, 1);
+  data.dewpoint = commonSensorResult.average;
+  data.dewpointMin = commonSensorResult.min;
+  data.dewpointMax = commonSensorResult.max;
+
+  commonSensorResult = await commonDaySensorQuery(db, "outHumidity", from, to, minValues, (value, precision) => utils.round(value, precision), 0);
+  data.outHumidity = commonSensorResult.average;
+  data.outHumidityMin = commonSensorResult.min;
+  data.outHumidityMax = commonSensorResult.max;
+
+  commonSensorResult = await commonDaySensorQuery(db, "barometer", from, to, minValues, unitConverter.inHgToHpa, 1);
+  data.barometer = commonSensorResult.average;
+  data.barometerMin = commonSensorResult.min;
+  data.barometerMax = commonSensorResult.max;
+
+  data.windspeed = await getDayAverage(db, "windSpeed", from, to);
+  data.windspeed = shortenAverage(data.windspeed, minValues).map((value) => unitConverter.mphToKmh(value, 1));
+
+  data.windspeedMax = await getDayMax(db, "windGust", from, to);
+  data.windspeedMax = shortenMax(data.windspeedMax, minValues).map((value) => unitConverter.mphToKmh(value, 1));
+
+  data.windDir = await getDayAverage(db, "windDir", from, to);
+  data.windDir = shortenAverage(data.windDir, minValues).map((value) => utils.round(value, 0));
+
+  data.rain = await getDaySum(db, "rain", from, to);
+  data.rain = shortenSum(data.rain, minValues);
+  data.rain = accumulateArray(data.rain).map((value) => unitConverter.inchToMm(value, 1));
 
   return data;
 }
@@ -158,6 +219,28 @@ async function getDayMax(db: Database, sensor: string, fromUnix: number, toUnix:
 
   const result = await db.query("weewx", `SELECT max FROM archive_day_${sensor} WHERE dateTime BETWEEN ? AND ? ORDER BY dateTime ASC`, [from.getTime() / 1000, to.getTime() / 1000]);
   return result.map((row: { max: number }) => row.max);
+}
+
+async function getDayAverage(db: Database, sensor: string, fromUnix: number, toUnix: number): Promise<number[]> {
+  const from = new Date(fromUnix * 1000);
+  from.setHours(0, 0, 0, 0);
+
+  const to = new Date(toUnix * 1000);
+  to.setHours(0, 0, 0, 0);
+
+  const result = await db.query("weewx", `SELECT sum, count FROM archive_day_${sensor} WHERE dateTime BETWEEN ? AND ? ORDER BY dateTime ASC`, [from.getTime() / 1000, to.getTime() / 1000]);
+  return result.map((row: { sum: number; count: number }) => row.sum / row.count);
+}
+
+async function getDaySum(db: Database, sensor: string, fromUnix: number, toUnix: number): Promise<number[]> {
+  const from = new Date(fromUnix * 1000);
+  from.setHours(0, 0, 0, 0);
+
+  const to = new Date(toUnix * 1000);
+  to.setHours(0, 0, 0, 0);
+
+  const result = await db.query("weewx", `SELECT sum FROM archive_day_${sensor} WHERE dateTime BETWEEN ? AND ? ORDER BY dateTime ASC`, [from.getTime() / 1000, to.getTime() / 1000]);
+  return result.map((row: { sum: number }) => row.sum);
 }
 
 function generateDatetimes(from: number, to: number, interval: number): number[] {
@@ -226,6 +309,44 @@ function shortenSum(array: number[], recommendedLength: number): number[] {
   return newArray;
 }
 
+function shortenMin(array: number[], recommendedLength: number): number[] {
+  const stepSize = calculateStepSize(array.length, recommendedLength);
+  const newArray: number[] = [];
+  let min = Number.MAX_SAFE_INTEGER;
+  let i = 0;
+  for (const value of array) {
+    if (value < min) {
+      min = value;
+    }
+    if (i % stepSize === 0) {
+      newArray.push(min);
+      min = Number.MAX_SAFE_INTEGER;
+    }
+    i++;
+  }
+
+  return newArray;
+}
+
+function shortenMax(array: number[], recommendedLength: number): number[] {
+  const stepSize = calculateStepSize(array.length, recommendedLength);
+  const newArray: number[] = [];
+  let max = Number.MIN_SAFE_INTEGER;
+  let i = 0;
+  for (const value of array) {
+    if (value > max) {
+      max = value;
+    }
+    if (i % stepSize === 0) {
+      newArray.push(max);
+      max = Number.MIN_SAFE_INTEGER;
+    }
+    i++;
+  }
+
+  return newArray;
+}
+
 function accumulateArray(array: number[]): number[] {
   if (array.length === 0) {
     return [];
@@ -253,13 +374,13 @@ function assignValues(datetimes: number[], $values: number[], $valuedatetimes: n
     if (valueMap.hasOwnProperty(datetime)) {
       newValues.push(valueMap[datetime]);
     } else {
-      newValues.push(Infinity);
+      newValues.push(Number.MAX_SAFE_INTEGER);
     }
   }
 
-  if (newValues[0] === Infinity) {
+  if (newValues[0] === Number.MAX_SAFE_INTEGER) {
     for (let i = 1; i < newValues.length; i++) {
-      if (newValues[i] !== Infinity) {
+      if (newValues[i] !== Number.MAX_SAFE_INTEGER) {
         newValues[0] = newValues[i];
         break;
       }
@@ -267,7 +388,7 @@ function assignValues(datetimes: number[], $values: number[], $valuedatetimes: n
   }
 
   for (let i = 1; i < newValues.length; i++) {
-    if (newValues[i] === Infinity) {
+    if (newValues[i] === Number.MAX_SAFE_INTEGER) {
       newValues[i] = newValues[i - 1];
     }
   }
@@ -298,4 +419,40 @@ function assignValuesSum(datetimes: number[], values: number[], valuedatetimes: 
   }
 
   return newValues;
+}
+
+async function getDayDatetimes(db: Database, fromUnix: number, toUnix: number): Promise<number[]> {
+  const from = new Date(fromUnix * 1000);
+  from.setHours(0, 0, 0, 0);
+  const to = new Date(toUnix * 1000);
+  to.setHours(0, 0, 0, 0);
+
+  const result = await db.query("weewx", "SELECT dateTime FROM archive_day_outTemp WHERE dateTime BETWEEN ? AND ? ORDER BY dateTime ASC", [from.getTime() / 1000, to.getTime() / 1000]);
+  if (!result) {
+    return [];
+  }
+  const data: number[] = [];
+  for (const row of result) {
+    data.push(row.dateTime);
+  }
+  return data;
+}
+
+async function commonDaySensorQuery(db: Database, sensor: string, from: number, to: number, minValues: number, converter: (number: number, precision: number) => number, precision: number): Promise<DayCommonSensorRow> {
+  const data: DayCommonSensorRow = {
+    average: [],
+    min: [],
+    max: [],
+  };
+
+  data.average = await getDayAverage(db, sensor, from, to);
+  data.average = shortenAverage(data.average, minValues).map((value) => converter(value, precision));
+
+  data.min = await getDayMin(db, sensor, from, to);
+  data.min = shortenMin(data.min, minValues).map((value) => converter(value, precision));
+
+  data.max = await getDayMax(db, sensor, from, to);
+  data.max = shortenMax(data.max, minValues).map((value) => converter(value, precision));
+
+  return data;
 }
